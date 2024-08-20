61fc007580a52ca3c48dc634759cd6b96ad719fd
adric@umich.edu <adric@umich.edu>
2024-08-20 16:04:19 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +12,21 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Set 'name' to the text in the 'name_box'
+    name = self.name_box.text
+    # Set 'email' to the text in the 'email_box'
+    email = self.email_box.text
+    # Set 'feedback' to the text in the 'feedback_box'
+    feedback = self.feedback_box.text
+    alert("You clicked the button.\n" + name + "\n" + email + "\n" + feedback) 
+    Notification("Submitted!").show()
+    self.clear_inputs()
+  
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
